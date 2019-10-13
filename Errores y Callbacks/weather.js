ea67896a20bfe9c3275c3fa401fc95bb665ad077
emifervi <emifervi@hotmail.com>
2019-10-13 13:03:46 -0500

    lab 5

---
+++ Errores y Callbacks/weather.js
@@ -0,0 +1,37 @@
+const credentials = require('./credentials')
+const request = require('request')
+
+const getWeather = (place) => {
+  const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=${credentials.MAPBOX_TOKEN}`
+
+  request({url:mapUrl, json:true}, (error, response, body) => {
+    if(error) {
+      return console.log(error);
+    }
+    if(response.statusCode >= 400) {
+      return console.log(body.message);
+    }
+    const location = body.features[0]
+    const long = location.center[0]
+    const lat = location.center[1]
+
+    const weatherUrl = `https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`
+
+    request({url:weatherUrl, json:true}, (error, response, body) => {
+      if(error) {
+        return console.log(error);
+      }
+      if(response.statusCode >= 400) {
+        return console.log(body.error);
+      }
+      const temp = body.currently.temperature
+      const sumary = body.currently.summary
+      const precipProb = body.currently.precipProbability
+      console.log(sumary + ". Actualmente esta a " + temp + "°C" + ". Hay " + precipProb * 100 + "% de posibilidad de lluvia.");
+    });
+  });
+}
+
+module.exports = {
+  getWeather : getWeather
+}
