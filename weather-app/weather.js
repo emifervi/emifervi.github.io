84a64c859d5041eded106e696808f1630e820d29
emifervi <emifervi@hotmail.com>
2019-11-25 17:01:29 -0600

    add front for exam

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -2,16 +2,19 @@ const credentials = require('./credentials')
 const request = require('request')
 
 const getWeather = (place) => {
-  const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=${credentials.MAPBOX_TOKEN}`
+  const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=123`
 
   request({url:mapUrl, json:true}, (error, response, body) => {
     const location = body.features[0]
     const long = location.center[0]
     const lat = location.center[1]
 
-    const weatherUrl = `https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`
+    const weatherUrl = `https://api.darksky.net/forecast/${12345}/${lat},${long}?lang=es&units=si`
 
     request({url:weatherUrl, json:true}, (error, response, body) => {
+      if(error) {
+        return console.log(error)
+      }
       const temp = body.currently.temperature
       const sumary = body.currently.summary
       const precipProb = body.currently.precipProbability
